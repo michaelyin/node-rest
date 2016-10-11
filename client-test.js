7bc7f20318e7f16b5c423186c1b2ac7b2f91a557
michaelyin <gt8442b@yahoo.com>
2016-10-11 16:54:55 -0400

    fix testing data.

--- client-test.js
+++ client-test.js
@@ -1,5 +1,6 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
+var fs = require("fs");
 var req = new XMLHttpRequest();
 req.open('POST', 'http://127.0.0.1:8081/asciimath');
 req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
@@ -8,7 +9,19 @@ req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 var mathml;
 var tpl = fs.readFile( __dirname + "/" + "mathml.json", 'utf8', function (err, data) {
 	mathml = JSON.parse(data);
+	console.log("============> send out: ")
+	console.log(mathml);
+	req.send(JSON.stringify(mathml));
 })
+
+req.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var response = this.responseText;
+    // use response in here.
+    console.log("<============ get back response:");
+    console.log(response);
+  }
+}
 
 /*
 var mathml= {
@@ -18,4 +31,4 @@ var mathml= {
 };
 */
 
-req.send(JSON.stringify(mathml));
+
